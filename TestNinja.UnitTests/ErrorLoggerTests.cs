c4a3f3b2307f112b4304b3af87825f0ef427a9c0
Stuart Bowes <stuartbowes2000@gmail.com>
2020-03-16 13:56:07 +0000

    Add ErrorLogger tests (Testing a void method that throws exceptions and raises an event.

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            //    logger.Log(error);
+
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+
+        }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+
+            var id = Guid.Empty;
+
+            // subscribe to ErrorLogged event handler
+            logger.ErrorLogged += (sender, args) => { id = args; }; // set id to Guid value 
+
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
+    }
+}
