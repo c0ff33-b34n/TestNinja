5acd2a0cab1b6eab72051a24ff768fc041a6c383
Stuart Bowes <stuartbowes2000@gmail.com>
2020-03-25 10:16:49 +0000

    Refactor VideoService to extract code which touches external resources

--- TestNinja/Mocking/VideoService.cs
+++ TestNinja/Mocking/VideoService.cs
@@ -9,9 +9,19 @@ namespace TestNinja.Mocking
 {
     public class VideoService
     {
-        public string ReadVideoTitle(IFileReader fileReader)
+        private IFileReader _fileReader;
+        private IVideoRepository _repository;
+
+        public VideoService(IFileReader fileReader = null, IVideoRepository repository = null)
         {
-            var str = fileReader.Read("video.txt");
+            _fileReader = fileReader ?? new FileReader();
+            _repository = repository ?? new VideoRepository();
+        }
+
+
+        public string ReadVideoTitle()
+        {
+            var str = _fileReader.Read("video.txt");
             var video = JsonConvert.DeserializeObject<Video>(str);
             if (video == null)
                 return "Error parsing the video.";
@@ -21,19 +31,13 @@ namespace TestNinja.Mocking
         public string GetUnprocessedVideosAsCsv()
         {
             var videoIds = new List<int>();
-            
-            using (var context = new VideoContext())
-            {
-                var videos = 
-                    (from video in context.Videos
-                    where !video.IsProcessed
-                    select video).ToList();
-                
-                foreach (var v in videos)
-                    videoIds.Add(v.Id);
 
-                return String.Join(",", videoIds);
-            }
+            var videos = _repository.GetUnprocessedVideos();
+            foreach (var v in videos)
+                videoIds.Add(v.Id);
+
+            return String.Join(",", videoIds);
+        
         }
     }
 
