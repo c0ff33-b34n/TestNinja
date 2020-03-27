005c7e2e47820fabdd4807ec3f93e16cda344f6f
Stuart Bowes <stuartbowes2000@gmail.com>
2020-03-27 13:27:15 +0000

    Add unit test for exception in HousekeeperServiceTests

--- TestNinja.UnitTests/Mocking/HousekeeperServiceTests.cs
+++ TestNinja.UnitTests/Mocking/HousekeeperServiceTests.cs
@@ -121,6 +121,21 @@ namespace TestNinja.UnitTests.Mocking
             VerifyEmailNotSent();
         }
 
+        [Test]
+        public void SendStatementEmails_EmailSendingFails_DisplayMessageBox()
+        {
+            _emailSender.Setup(es => es.EmailFile(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>()
+                )).Throws<Exception>();
+
+            _service.SendStatementEmails(_statementDate);
+
+            _messageBox.Verify(mb => mb.Show(It.IsAny<string>(), It.IsAny<string>(), MessageBoxButtons.OK));
+        }
+
         private void VerifyEmailNotSent()
         {
             _emailSender.Verify(es => es.EmailFile(
