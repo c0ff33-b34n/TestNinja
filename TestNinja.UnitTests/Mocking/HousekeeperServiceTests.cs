bea19b308fca481f2505b0787589dc1f4384e60a
Stuart Bowes <stuartbowes2000@gmail.com>
2020-03-27 10:11:46 +0000

    Refactor and rename HousekeeperHelper and add first unit test

---
+++ TestNinja.UnitTests/Mocking/HousekeeperServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+using static TestNinja.Mocking.HousekeeperService;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    class HousekeeperServiceTests
+    {
+        [Test]
+        public void SendStatementEmails_WhenCalled_GenerateStatements()
+        {
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(uow => uow.Query<Housekeeper>()).Returns(new List<Housekeeper>
+            {
+                new Housekeeper { Email = "a", FullName = "b", Oid = 1, StatementEmailBody = "c" }
+            }.AsQueryable());
+
+            var statementGenerator = new Mock<IStatementGenerator>();
+            var emailSender = new Mock<IEmailSender>();
+            var messageBox = new Mock<IXtraMessageBox>();
+
+            var service = new HousekeeperService(
+                    unitOfWork.Object,
+                    statementGenerator.Object,
+                    emailSender.Object,
+                    messageBox.Object);
+
+            service.SendStatementEmails(new DateTime(2020, 03, 27));
+
+            statementGenerator.Verify(sg => sg.SaveStatement(1, "b", new DateTime(2020, 03, 27)));
+
+
+        }
+    }
+}
